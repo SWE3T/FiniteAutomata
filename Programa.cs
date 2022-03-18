fa1f3fabb57590618d701f997e064b9efb71cefc
Guilherme <guilherme.devon@hotmail.com>
2022-03-17 23:28:13 -0300

    New: Split de atribuição

--- Programa.cs
+++ Programa.cs
@@ -23,11 +23,17 @@ namespace DETERMINADOR
 
             Console.WriteLine("\nLeitura do Arquivo: ");
 
-            for (var line = 0; line < lines.Length - 1; line++)
+            for (var line = 0; line < lines.Length; line++)
             {
-                if (Regex.IsMatch(lines[line], @"[a-z]+")) //caso o caractere seja um simbolo
+                if (Regex.IsMatch(lines[line], @"[a-zã]+")) //caso o caractere seja um simbolo
                 {
                     Console.WriteLine(lines[line]); // Debug
+                    //Console.WriteLine(lines[line].Split("::=").Length);
+                
+                    var result = lines[line].Split("::=").Length == 2 ? "Atribuição de estado" : "Palavra reservada";
+                    Console.WriteLine(result + '\n');
+                    //split por |; para ter o estado alterado e
+                    //replace  <X> por ' '; para separar os simbolos
                 }
             }
         }
