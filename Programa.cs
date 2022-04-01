80f415b062ef830466369f7dd903313e954315b4
Guilherme <guilherme.devon@hotmail.com>
2022-04-01 01:18:52 -0300

    Pronto :)

--- Programa.cs
+++ Programa.cs
@@ -46,16 +46,14 @@ namespace DETERMINADOR {
                 if (item.Contains("<")) {
                 string[] symbols = item.Split("<");
                   string temp = GetRow(AFND, 0, 1); 
-                  if (temp.Contains(symbols[0])) {
+                  if (temp.Contains(symbols[0])) { //Caso o símbolo esteja na matriz
                     string indexOfSymbol = string.Concat(temp.Where(c =>!char.IsWhiteSpace(c)));
                     int indexOfSymbolOnTable = indexOfSymbol.IndexOf(symbols[0].Replace(" ", String.Empty));
 
                     string[] State = symbols[1].Split(">");
-                  
-                    
+                          
                     string temp2 = GetColumn(AFND, 0, 1);
-                    
-
+                
                     if (!temp2.Contains((char)(State[0].ToCharArray()[0] + (stateHandlerCounter - 1)))) { //Caso o estado NÃO esteja na matriz
                       ResizeArray(ref AFND, AFND.GetLength(0) + 1, AFND.GetLength(1)); //Função para aumentar o tamanho da matriz
                       AFND[AFND.GetLength(0) - 1, 0] = (char)(State[0].ToCharArray()[0] + (stateHandlerCounter - 1)) + ""; //Adiciona o estado na matriz
@@ -64,22 +62,25 @@ namespace DETERMINADOR {
                     temp2 = GetColumn(AFND, 0, 1);
                     string indexOfSymbol2 = temp2.Replace(" ", String.Empty);
                     indexOfSymbol2 = indexOfSymbol2.Replace("*", String.Empty);
-                    int indexOfSymbolOnTable2 = indexOfSymbol2.IndexOf(State[0].Replace("*", String.Empty));
-                    AFND[(indexOfSymbolOnTable2+1) + (stateHandlerCounter-1), indexOfSymbolOnTable + 1] = AFND[indexOfSymbolOnTable2+1 + (stateHandlerCounter-1), indexOfSymbolOnTable + 1] + (char)(65 + ((int) State[0].ToCharArray()[0] - 65) + (stateHandlerCounter - 1)) + ", ";
+                    int indexOfSymbolOnTable2 = indexOfSymbol2.IndexOf(nextStates[0].ToCharArray()[1]);
+                    AFND[(indexOfSymbolOnTable2+1) + (stateHandlerCounter-1), indexOfSymbolOnTable + 1] = AFND[(indexOfSymbolOnTable2+ 1) + (stateHandlerCounter-1), indexOfSymbolOnTable + 1] + (char)(65 + ((int) State[0].ToCharArray()[0] - 65) + (stateHandlerCounter - 1)) + ", ";
                   }
-                  else {
+                  else { //Caso o símbolo NÃO esteja na matriz
                     ResizeArray(ref AFND, AFND.GetLength(0), AFND.GetLength(1) + 1); //Função para aumentar o tamanho da matriz
 
                     AFND[0, AFND.GetLength(1) - 1] = symbols[0].Replace(" ", String.Empty) + ""; //Adiciona o Simbolo novo no topo da matriz
                     string[] State = symbols[1].Split(">");
 
-                    AFND[1, AFND.GetLength(1) - 1] = (char)(
-                    65 + ((int) State[0].ToCharArray()[0] - 65) + stateHandlerCounter) + ", ";
                     string temp2 = GetColumn(AFND, 0, 1);
+                    string indexOfSymbol2 = temp2.Replace(" ", String.Empty);
+                    indexOfSymbol2 = indexOfSymbol2.Replace("*", String.Empty);
+                    int indexOfSymbolOnTable2 = indexOfSymbol2.IndexOf(State[0].Replace("*", String.Empty));
+
+                    AFND[indexOfSymbolOnTable2+1, AFND.GetLength(1) - 1] = (char)(65 + ((int) State[0].ToCharArray()[0] - 65) + (stateHandlerCounter-1)) + ", ";
 
                     if (!temp2.Contains((char)(State[0].ToCharArray()[0] + (stateHandlerCounter - 1)))) { //Caso o estado já esteja na matriz
                       ResizeArray(ref AFND, AFND.GetLength(0) + 1, AFND.GetLength(1)); //Função para aumentar o tamanho da matriz
-                      AFND[AFND.GetLength(0) - 1, 0] = (char)(State[0].ToCharArray()[0] + stateHandlerCounter) + ""; //Adiciona o estado na matriz
+                      AFND[AFND.GetLength(0) - 1, 0] = (char)(State[0].ToCharArray()[0] + (stateHandlerCounter-1)) + ""; //Adiciona o estado na matriz
                     }
                   }
                 }
@@ -112,13 +113,14 @@ namespace DETERMINADOR {
                     AFND[0, AFND.GetLength(1) - 1] = symbols[0].Replace(" ", String.Empty) + ""; //Adiciona o Simbolo novo no topo da matriz
                     string[] State = symbols[1].Split(">");
 
-                    AFND[1, AFND.GetLength(1) - 1] = (char)(65 + ((int) State[0].ToCharArray()[0] - 65) + (stateHandlerCounter - 1)) + ", "; //Adiciona o ESTADO novo no topo da matriz
                     string temp2 = GetColumn(AFND, 0, 1);
-
                     if (!temp2.Contains((char)(State[0].ToCharArray()[0] + stateHandlerCounter - 1))) { //Caso o estado NÃO  esteja na matriz
                       ResizeArray(ref AFND, AFND.GetLength(0) + 1, AFND.GetLength(1)); //Função para aumentar o tamanho da matriz
                       AFND[AFND.GetLength(0) - 1, 0] = (char)(State[0].ToCharArray()[0] + (stateHandlerCounter - 1)) + ""; //Adiciona o estado na matriz
                     }
+
+                    AFND[1, AFND.GetLength(1) - 1] = (char)(65 + ((int) State[0].ToCharArray()[0] - 65) + (stateHandlerCounter - 1)) + ", "; //Adiciona o ESTADO novo no topo da matriz
+
                   }
                 }
               }
@@ -181,63 +183,65 @@ namespace DETERMINADOR {
       }
       PrintAF(ref AFND);
       Console.WriteLine();  
+      Console.WriteLine();  
+      Console.WriteLine();  
       Console.WriteLine("Determinização");
 
       determinize(ref AFND);
+      setErrorState(ref AFND);
       PrintAF(ref AFND);
     }
 
     private static void determinize(ref string[, ] AFND) {
-      bool changes = false;
-      
       for (int i = 1; i < AFND.GetLength(0); i++) {
         for (int j = 1; j < AFND.GetLength(1); j++) {
-          if (AFND[i,j] != null && AFND[i,j].Contains(",") && !(AFND[i,j].Contains("[")))
-          {
+          if (AFND[i,j] != null && AFND[i,j].Contains(",") && !(AFND[i,j].Contains("["))) {
             string states2 = AFND[i, j].Replace(" ", String.Empty);
             string[] states = (states2.Split(','));
-            if (states.Length >= 3)
-            {
+            if (states.Length >= 3) {
                 AFND[i,j] = "[" + AFND[i,j].Remove(AFND[i,j].Length - 2) + "]";
-                ResizeArray(ref AFND, AFND.GetLength(0) + 1, AFND.GetLength(1)); //Função para aumentar o tamanho da matriz
-                //newStates = AFND[i,j].Remove(AFND[i,j].Length - 2);
-                AFND[AFND.GetLength(0)-1, 0] = AFND[i,j];
-
-                foreach (var state in states)
+                if (!GetColumn(AFND, 0, 1).Contains(AFND[i,j]))
                 {
-                    if (!(state == states.Last()))
+                    ResizeArray(ref AFND, AFND.GetLength(0) + 1, AFND.GetLength(1)); //Função para aumentar o tamanho da matriz
+                    //newStates = AFND[i,j].Remove(AFND[i,j].Length - 2);
+                    AFND[AFND.GetLength(0)-1, 0] = AFND[i,j];
+                
+                    foreach (var state in states)
                     {
-                        string temp = GetColumn(AFND, 0, 1);
-                        string indexOfState1 = temp.Replace(" ", String.Empty);
-                        string indexOfState = indexOfState1.Replace("*", String.Empty);
-                        int indexOfStateOnTable = indexOfState.IndexOf(state);
-                        
-                        Console.WriteLine(indexOfStateOnTable);
-                        if (AFND[indexOfStateOnTable+1, 0].Contains("*"))
-                        { //Passa o estado final para o novo estado criado;
-                            Console.WriteLine(AFND[AFND.GetLength(0)-1, 0]);
-                            AFND[AFND.GetLength(0)-1, 0] = "*" + AFND[AFND.GetLength(0)-1, 0];
-                        }
-
-                        for (var symbol = 1; symbol < AFND.GetLength(1); symbol++)
+                        if (!(state == states.Last()))
                         {
-                            AFND[AFND.GetLength(0)-1, symbol] = AFND[AFND.GetLength(0)-1, symbol] + AFND[indexOfStateOnTable+1, symbol]; //Console.Write(AFND[indexOfStateOnTable+1, symbol]);
-                        }             
+                            string temp = GetColumn(AFND, 0, 1);
+                            string indexOfState1 = temp.Replace(" ", String.Empty);
+                            string indexOfState = indexOfState1.Replace("*", String.Empty);
+                            int indexOfStateOnTable = indexOfState.IndexOf(state);
+                            
+                            if (AFND[indexOfStateOnTable+1, 0].Contains("*") && !(AFND[AFND.GetLength(0)-1, 0].Contains("*")))
+                            { //Passa o estado final para o novo estado criado;
+                                AFND[AFND.GetLength(0)-1, 0] = "*" + AFND[AFND.GetLength(0)-1, 0];
+                            }
 
-                        //AFND[indexOfStateOnTable, 0] = "*" + AFND[indexOfStateOnTable, 0];
-                        //tem que achar o index e fazer um loop para adicionar em AFND[AFND.GetLength(0)-1, i] = AFND[index do estado, i];
-                        //tem que chcar se algum é estado final
+                            for (var symbol = 1; symbol < AFND.GetLength(1); symbol++)
+                            {
+                                AFND[AFND.GetLength(0)-1, symbol] = AFND[AFND.GetLength(0)-1, symbol] + AFND[indexOfStateOnTable+1, symbol]; //Console.Write(AFND[indexOfStateOnTable+1, symbol]);
+                            }             
+                    
+                        }
                     }
                 }
             }
-            changes = false;
           }
         }
       }
+    }
 
-      if (changes)
-      { //Recursividade para percorer a matriz até não haver mais mudanças.
-          determinize(ref AFND);
+    private static void setErrorState(ref string[, ] AFND) {
+      for (int i = 1; i <= AFND.GetLength(0)-1; i++) {
+        for (int j = 1; j <= AFND.GetLength(1)-1; j++) {
+            if (!(AFND[i,j] == null) && AFND[i,j].Length == 0 || (AFND[i,j] == null)) //Que crise meus amigos
+            {
+                AFND[i,j] = "<erro>";
+            }
+        }
       }
     }
 
